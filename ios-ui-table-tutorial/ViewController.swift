6527792f7e85a5a2e826b6a84fb1120bd62fcbe9
Narongsak <narongsak.chobsri@gmail.com>
2017-12-16 15:36:15 +0700

    Add section and row

--- ios-ui-table-tutorial/ViewController.swift
+++ ios-ui-table-tutorial/ViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,
+    UITableViewDelegate {
     
     @IBOutlet weak var mTableview:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "Facelespedia ."
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
